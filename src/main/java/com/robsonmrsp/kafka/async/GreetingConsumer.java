3451b180f04fe789fb235417b9af5eedaf851465
Marcio Robson <robsonmrsp@gmail.com>
2022-03-15 23:18:21 -0300

    Sending a avro payload and reseiving as well

--- src/main/java/com/robsonmrsp/kafka/async/GreetingConsumer.java
+++ src/main/java/com/robsonmrsp/kafka/async/GreetingConsumer.java
@@ -3,15 +3,19 @@ package com.robsonmrsp.kafka.async;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+
+import com.robsonmrsp.kafka.avro.GreetEvent;
 
 @Component
 public class GreetingConsumer {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(GreetingConsumer.class);
 
-	@KafkaListener(topics = "greeting-topic")
-	public void consume(String message) {
+	@KafkaListener(topics = "greeting-topic-avro")
+	public void consume(@Payload GreetEvent message) {
+
 		LOGGER.info("Receiving-> {}", message);
 	}
 }
--- src/main/java/com/robsonmrsp/kafka/async/GreetingProducer.java
+++ src/main/java/com/robsonmrsp/kafka/async/GreetingProducer.java
@@ -1,11 +1,21 @@
 package com.robsonmrsp.kafka.async;
 
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Random;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
 
+import com.robsonmrsp.kafka.avro.GreetEvent;
 import com.robsonmrsp.kafka.model.Greet;
 
 @Component
@@ -14,12 +24,32 @@ public class GreetingProducer {
 	private static final Logger LOGGER = LoggerFactory.getLogger(GreetingProducer.class);
 
 	@Autowired
-	KafkaTemplate<String, String> kafkaTemplate;
+	KafkaTemplate<Integer, GreetEvent> kafkaTemplate;
 
 	public void send(Greet g) {
+		Integer key = new Random(new Date().getTime()).nextInt();
+		GreetEvent event = new GreetEvent(g.getId().toString(), g.getMessage(), g.getToWhom());
 
 		String message = String.format("Bom dia %s-id[%s] %s!", g.getMessage(), g.getId().toString(), g.getToWhom());
 		LOGGER.info("Sending-> {}", message);
-		kafkaTemplate.send("greeting-topic", g.getId().toString(), message);
+
+		ListenableFuture<SendResult<Integer, GreetEvent>> send = kafkaTemplate.send(createMessageWithHeaders(key, event, "greeting-topic-avro"));
+		send.addCallback(a -> {
+			LOGGER.info("Sent: {}", a.getProducerRecord().topic());
+		}, b -> {
+			LOGGER.error("Error sending message: {}", b.getLocalizedMessage());
+		});
+	}
+
+	private Message<GreetEvent> createMessageWithHeaders(Integer messageId, GreetEvent event, String topic) {
+		return MessageBuilder.withPayload(event)
+				.setHeader("hash", event.hashCode())
+				.setHeader("version", "1.0.0")
+				.setHeader("endOfLife", LocalDate.now().plusDays(1L))
+				.setHeader("type", "fct")
+				.setHeader("cid", messageId)
+				.setHeader(KafkaHeaders.TOPIC, topic)
+				.setHeader(KafkaHeaders.MESSAGE_KEY, messageId)
+				.build();
 	}
 }
