9d9d715d16c3142c8fd01f98f1b68c903b7046b1
Marcio Robson <robsonmrsp@gmail.com>
2022-03-17 00:04:58 -0300

    Add java bean to configure schema registry

--- src/main/java/com/robsonmrsp/kafka/config/KafkaConfiguration.java
+++ src/main/java/com/robsonmrsp/kafka/config/KafkaConfiguration.java
@@ -6,25 +6,30 @@ import java.util.Map;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
 
 @Configuration
 //@Profile("dev")
 public class KafkaConfiguration {
 
+	@Autowired
+	KafkaProperties kafkaProperties;
+
 	@Bean
 	public Map<String, Object> producerConfigs() {
-		Map<String, Object> props = new HashMap<>(new KafkaProperties().buildProducerProperties());
+		Map<String, Object> props = new HashMap<>(kafkaProperties.buildProducerProperties());
 		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
 		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
 		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://0.0.0.0:8081");
 
 		return props;
 	}
