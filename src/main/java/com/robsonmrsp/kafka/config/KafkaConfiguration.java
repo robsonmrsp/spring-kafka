751fe7596ea28a545c61fb952b0234ad3cb0b477
Marcio Robson <robsonmrsp@gmail.com>
2022-03-16 23:40:04 -0300

    Add first kafka java config

--- src/main/java/com/robsonmrsp/kafka/config/KafkaConfiguration.java
+++ src/main/java/com/robsonmrsp/kafka/config/KafkaConfiguration.java
@@ -1,24 +1,49 @@
 package com.robsonmrsp.kafka.config;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
+import com.fasterxml.jackson.databind.ser.std.NumberSerializers.IntegerSerializer;
+
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+
 @Configuration
+@Profile("dev")
 public class KafkaConfiguration {
 
-//	@Bean
-//	public ProducerFactory<String, String> producerFactory() {
-//		return new DefaultKafkaProducerFactory<>(Map.of());
-//	}
-//
-//	@Bean
-//	public KafkaTemplate<String, String> kafkaTemplate() {
-//		return new KafkaTemplate<>(producerFactory());
-//
-//	}
+	@Bean
+	public Map<String, Object> producerConfigs() {
+		Map<String, Object> props = new HashMap<>(new KafkaProperties().buildProducerProperties());
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
+		return props;
+	}
+
+	@Bean
+	public ProducerFactory<String, Object> producerFactory() {
+		return new DefaultKafkaProducerFactory<>(producerConfigs());
+	}
+
+	@Bean
+	public KafkaTemplate<String, Object> kafkaTemplate() {
+		return new KafkaTemplate<>(producerFactory());
+	}
+
+	@Bean
+	public NewTopic greetTopic() {
+		return new NewTopic("greet-topic-avro", 3, (short) 1);
+	}
+
 }
